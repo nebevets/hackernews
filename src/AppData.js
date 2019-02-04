a00f3b900c2f7f293c55e5b50890c6635e600042
nebevets <steveben@exmsft.com>
2019-02-04 11:50:03 -0800

    implemented basic title search

---
+++ src/AppData.js
@@ -0,0 +1,44 @@
+const list = [
+    {
+      title: 'black elvis',
+      url: 'https://en.wikipedia.org/wiki/Kool_Keith',
+      author: 'kool keith',
+      num_comments: 7,
+      points: 999999,
+      objectID: 123456
+    },
+    {
+        title: 'depeche mode',
+        url: 'http://www.depechemode.com',
+        author: 'kool keith',
+        num_comments: 1981,
+        points: 2019,
+        objectID: 473829
+    },
+    {
+        title: 'original fire',
+        url: 'http://www.meatbeatmanifesto.com',
+        author: 'jack dangers',
+        num_comments: 50,
+        points: 678,
+        objectID: 203948
+    },
+    {
+        title: 'autechre',
+        url: 'http://www.bleep.com',
+        author: 'autechre',
+        num_comments: 777,
+        points: 1,
+        objectID: 564738
+    },
+    {
+      title: 'to live and die in l.a.',
+      url: 'http://www.latimes.com',
+      author: 'wang chung',
+      num_comments: 213,
+      points: 1988,
+      objectID: 654321
+    }
+];
+
+export default list;
